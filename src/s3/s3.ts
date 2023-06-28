830672cd07084fd151c6d843010a6524ec661e8c
Reinaldy Rafli <aldy505@proton.me>
2023-06-28 17:24:22 +0700

    feat(s3): innitialize s3 provider implementation

--- src/s3/s3.ts
+++ src/s3/s3.ts
@@ -1,33 +1,86 @@
 import { type IObjectStorage, type PutOptions, type StatResponse } from "../interface";
 import { type ConnectionString } from "../connectionString";
 import { Readable, Writable } from "node:stream";
+import {
+    CopyObjectCommand,
+    DeleteObjectCommand,
+    GetObjectCommand, ListObjectsV2Command,
+    S3Client,
+    type S3ClientConfig
+} from "@aws-sdk/client-s3";
 
 export class S3Storage implements IObjectStorage {
+    private readonly client: S3Client;
+    private readonly bucketName: string;
     constructor(config: ConnectionString) {
+        let clientConfig: S3ClientConfig = {}
+        this.client = new S3Client(clientConfig);
+        this.bucketName = config.bucketName;
     }
 
-    copy(sourcePath: string, destinationPath: string): Promise<void> {
-        return Promise.resolve(undefined);
+    async copy(sourcePath: string, destinationPath: string): Promise<void> {
+        const command = new CopyObjectCommand({
+            Bucket: this.bucketName,
+            CopySource: sourcePath,
+            Key: destinationPath
+        });
+
+        await this.client.send(command);
     }
 
-    delete(path: string): Promise<void> {
-        return Promise.resolve(undefined);
+    async delete(path: string): Promise<void> {
+        const command = new DeleteObjectCommand({
+            Bucket: this.bucketName,
+            Key: path
+        });
+
+        await this.client.send(command);
     }
 
     exists(path: string): Promise<boolean> {
         return Promise.resolve(false);
     }
 
-    get(path: string, encoding?: string): Promise<Buffer> {
-        return Promise.resolve(undefined);
+    async get(path: string, encoding?: string): Promise<Buffer> {
+        const command = new GetObjectCommand({
+            Bucket: this.bucketName,
+            Key: path,
+        });
+
+        const response = await this.client.send(command);
+
+        if (response.Body == null) {
+            throw new Error("this can't be happening");
+        }
+
+        const byteArray = await response.Body.transformToByteArray();
+        return Buffer.from(byteArray.buffer);
     }
 
-    getStream(path: string): Promise<Readable> {
-        return Promise.resolve(undefined);
+    async getStream(path: string): Promise<Readable> {
+        return new Readable();
     }
 
-    list(path?: string): Promise<Iterable<string>> {
-        return Promise.resolve(undefined);
+    async list(path?: string): Promise<Iterable<string>> {
+        const command = new ListObjectsV2Command({
+            Bucket: this.bucketName,
+            Prefix: path
+        });
+        const response = await this.client.send(command);
+
+        if (response.Contents == null) {
+            throw new Error("handle this thing better")
+        }
+
+        const objects: string[] = [];
+
+        for (const object of response.Contents) {
+            if (object.Key == null) continue;
+
+            objects.push(object.Key);
+        }
+
+        return objects;
     }
 
     move(sourcePath: string, destinationPath: string): Promise<void> {
@@ -38,8 +91,8 @@ export class S3Storage implements IObjectStorage {
         return Promise.resolve(undefined);
     }
 
-    putStream(path: string, options?: PutOptions): Writable {
-        return undefined;
+    putStream(path: string, options?: PutOptions): Promise<Writable> {
+        return Promise.resolve(new Writable())
     }
 
     stat(path: string): Promise<StatResponse> {
