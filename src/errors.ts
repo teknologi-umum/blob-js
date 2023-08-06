0c63fdff12969ed8140eeb29fa8d8db95943cc4a
Reinaldy Rafli <aldy505@proton.me>
2023-08-06 13:02:48 +0700

    docs: detailed per function documentation (#6)

--- src/errors.ts
+++ src/errors.ts
@@ -1,18 +1,40 @@
+/**
+ * The BlobMismatchedMD5IntegrityError class represents an error that occurs when
+ * the MD5 hash of a blob does not match the expected hash
+ */
 export class BlobMismatchedMD5IntegrityError extends Error {
+    /**
+     * @param expecting The expected MD5 hash of the blob.
+     * @param acquired The MD5 hash of the blob that was actually acquired.
+     */
     constructor(public readonly expecting: string, public readonly acquired: string) {
         super(`Mismatched MD5 integrity check. Expecting ${expecting} while acquired ${acquired}`);
         this.name = "BlobMismatchedMD5IntegrityError";
     }
 }
 
+/**
+ * The BlobFileNotExistError class represents an error that occurs when the file
+ * that is being accessed does not exist.
+ */
 export class BlobFileNotExistError extends Error {
+    /**
+     * @param path The path of the file that does not exist.
+     */
     constructor(public readonly path: string) {
         super(`File not exists: ${path}`);
         this.name = "BlobFileNotExistError";
     }
 }
 
+/**
+ * The BlobFilePermissionDeniedError class represents an error that occurs when
+ * the user does not have permission to access the file that is being accessed.
+ */
 export class BlobFilePermissionDeniedError extends Error {
+    /**
+     * @param path The path of the file that the user does not have permission to access.
+     */
     constructor(public readonly path: string) {
         super(`Permission denied: ${path}`);
         this.name = "BlobFilePermissionDeniedError";
--- src/storage.ts
+++ src/storage.ts
@@ -1,21 +1,47 @@
+import { Readable, Writable } from "node:stream";
 import { type IObjectStorage, PutOptions, StatResponse } from "./interface";
 import { parseConnectionString } from "./connectionString";
 import { FileStorage } from "./file/file";
-import { Readable, Writable } from "node:stream";
+import { S3Storage } from "./s3/s3";
 
-
+/**
+ *
+ * The Storage class implements the IObjectStorage interface and provides a way to interact
+ * with an object storage service. The class takes a connection string as input and uses it
+ * to initialize the appropriate implementation class for the specified provider.
+ */
 export class Storage implements IObjectStorage {
     readonly #implementation: IObjectStorage;
 
+    /**
+     * The Storage constructor takes a connectionString as input and uses it to initialize
+     * the appropriate implementation class for the specified provider.
+     * The connectionString is a string that specifies the connection information for
+     * the object storage service. The format of the connectionString depends on the provider.
+     *
+     * The following are the supported providers and their respective connection string formats:
+     *
+     * * AWS (or any other S3-compatible storage): s3://access_key:secret_key@bucket_name?forcePathStyle=true&customUserAgent=your_user_agent&more_config=more_value
+     * * Google Cloud: gcs://bucket_name
+     * * Azure Blob: azblob://my_container
+     * * Filesystem: file:///path/to/directory
+     * Note: Using any other schemes than s3, gcs, azblob or file will throw a TypeError.
+     *
+     * For example, the following is a valid connectionString for AWS S3:
+     *
+     * s3://access_key:secret_key@my-bucket?forcePathStyle=true&customUserAgent=my_user_agent&more_config=more_value
+     * @param connectionString Connection string that specifies the connection information for the object storage service
+     * @throws TypeError Unknown or unsupported schemes/provider was given
+     */
     constructor(connectionString: string) {
         const parsedConnectionString = parseConnectionString(connectionString);
         switch (parsedConnectionString.provider) {
             case "file":
                 this.#implementation = new FileStorage(parsedConnectionString.bucketName);
                 break;
-            // case "s3":
-            //     this.#implementation = new S3Storage(parsedConnectionString);
-            //     break;
+            case "s3":
+                this.#implementation = new S3Storage(parsedConnectionString);
+                break;
             // case "azblob":
             //     this.#implementation = new AzureBlobStorage(parsedConnectionString);
             //     break;
